cae9fde762093b6c7fe29f6ce14c0a6bddceeff1
Gene Grady <grady.gene.thomas@gmail.com>
2019-12-14 14:46:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+findMatching=(arr, string)=>{
+    return arr.filter(name=>
+        name.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch(source, string){
+    return source.filter(name=>
+      name.toLowerCase().indexOf(string.toLowerCase()) ===0 )
+}
+
+function matchName(drivers, string){
+   return drivers.filter(name=> 
+    name.name===string)
+}
